2f6121427c98d8782474aa84f3233c402bfa2be9
hyeondle <st.linsio@gmail.com>
2025-04-12 15:07:01 +0900

    del

--- control-panel/src/test_transcribe.js
+++
@@ -1,5 +0,0 @@
-async function testTranscribe() {
-    const res = await fetch('/transcribe-test', { method: 'POST' });
-    const data = await res.json();
-    document.getElementById('transcription-result').innerText = data.text || data.error;
-}
